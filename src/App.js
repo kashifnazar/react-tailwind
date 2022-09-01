dbfcd1a065cb4055e756eea814da80928e708b02
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-09-01 13:08:08 +0100

    Setup of header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className='grid grid-cols-[1fr_4fr] gap-1'>
+    <div className='grid grid-cols-[1fr_4fr] gap-1 p-3'>
       <div className="col-span-2"><Header /></div>
       <div><NavBar /></div>
       <div><Body /></div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import {Bars3Icon, VideoCameraIcon, BellIcon, MagnifyingGlassIcon} from '@heroicons/react/24/outline'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-between items-center'>
+      <div className='flex items-center'>
+        <Bars3Icon className='w-10' />
+        YouTube Logo
+      </div>
+      <div className='flex border-gray-300 border-2 rounded-3xl px-4 py-2 items-center'>
+        <input type="text" placeholder='Search' className='border-transparent active:outline-none focus:outline-none' />
+        <div>
+            <MagnifyingGlassIcon className='w-5' />
+        </div>
+      </div>
+      <div className='flex flex-row'>
+        <VideoCameraIcon />
+        <BellIcon />
+      </div>
+    </div>
   )
 }
 
