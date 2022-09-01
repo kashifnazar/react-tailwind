f54af47a3fc60779acfa9bbcd859bd28c1015fd7
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-09-01 11:15:13 +0100

    Created top-level components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Body from "./components/Body";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className='grid grid-cols-[1fr_4fr] gap-1'>
-      <div className="col-span-2">Top bar</div>
-      <div className="">NavBar</div>
-      <div>Body</div>
+      <div className="col-span-2"><Header /></div>
+      <div><NavBar /></div>
+      <div><Body /></div>
     </div>
   );
 }
